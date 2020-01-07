9a2868160f4fdd9cb740284844e1398cd667c481
kymaka <seafth345@gmail.com>
2020-01-07 21:27:20 +0700

    WIP

--- task_2_2/src/main/java/ru/nsu/fit/g18214/shatalov/Elem.java
+++ task_2_2/src/main/java/ru/nsu/fit/g18214/shatalov/Elem.java
@@ -1,4 +1,45 @@
 package ru.nsu.fit.g18214.shatalov;
 
-public class Elem {
+class Elem<O, K> {
+
+  private O element;
+  Elem<O, K> nextElem;
+  Elem<O, K> preElem;
+  K key;
+
+  Elem(K k, O newElem, Elem<O, K> pre) {
+    preElem = pre;
+    element = newElem;
+    key = k;
+  }
+
+  void addNextElem(Elem<O, K> next) {
+    nextElem = next;
+    if (next != null) {
+      next.preElem = this;
+    }
+  }
+
+  O getElem() {
+    return element;
+  }
+
+  Elem<O, K> getPreElem() {
+    return preElem;
+  }
+
+  Elem<O, K> getNextElem() {
+    return nextElem;
+  }
+
+  Elem<O, K> createNextElem(K k, O next) {
+    if (nextElem == null) {
+      nextElem = new Elem<O, K>(k, next, this);
+    } else {
+      Elem<O, K> savedNext = nextElem;
+      nextElem = new Elem<O, K>(k, next, this);
+      nextElem.nextElem = savedNext;
+    }
+    return nextElem;
+  }
 }
