7ef72ee7c695e8fc70284d14a2848936b01cab6b
Ivan Shatalov <seafth345@gmail.com>
2020-07-26 18:13:53 +0700

    Im blue dabudu dabuday

--- Task_2_2_1/src/main/java/ru/nsu/fit/g18214/shatalov/Food.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/g18214/shatalov/Food.java
@@ -1,12 +1,36 @@
 package ru.nsu.fit.g18214.shatalov;
 
+
+import javafx.scene.image.Image;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Food {
   double x;
   double y;
+  Image food;
 
   public Food(double x, double y) {
     this.x = x;
     this.y = y;
+    try {
+      this.food = new Image(new FileInputStream(
+          "src/resources/food.png"
+      ));
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
   }
 
+  public Image getImage() {
+    return this.food;
+  }
+
+  public double getX() {
+    return this.x;
+  }
+  public double getY() {
+    return this.y;
+  }
 }
--- Task_2_2_1/src/main/java/ru/nsu/fit/g18214/shatalov/GameLoop.java
+++ Task_2_2_1/src/main/java/ru/nsu/fit/g18214/shatalov/GameLoop.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.g18214.shatalov;
 
 import javafx.application.Application;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
@@ -14,16 +15,59 @@ import javafx.util.Duration;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 
-public class GameLoop {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Random;
 
-  public void gameLoop() {
+public class GameLoop extends Application {
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) {
+
+    ArrayList<Food> foods = new ArrayList<>();
+
+    primaryStage.setTitle("Snake The Game");
+
     Group root = new Group();
     Scene theScene = new Scene(root);
+    primaryStage.setScene(theScene);
 
     Canvas canvas = new Canvas(512, 512);
     root.getChildren().add(canvas);
 
-    GraphicsContext gc = new canvas.getGraphicsContext2D();
+    GraphicsContext gc = canvas.getGraphicsContext2D();
+
+    Timeline gameLoop = new Timeline();
+    gameLoop.setCycleCount(Timeline.INDEFINITE);
+    Random r = new Random();
+    for (int i = 0; i < 5; i++) {
+      foods.add(new Food(512 * r.nextDouble(), 512 * r.nextDouble()));
+    }
+
+    final long timeStart = System.currentTimeMillis();
+
+    KeyFrame kf = new KeyFrame(
+        Duration.seconds(0.017),
+        new EventHandler<ActionEvent>() {
+          public void handle(ActionEvent actionEvent) {
+            gc.clearRect(0, 0, 512, 512);
+
+            for (int i = 0; i < 5; i++) {
+              //Image food = foods.get(i).getImage();
+              gc.drawImage(
+                  foods.get(i).getImage(), foods.get(i).getX(), foods.get(i).getY());
+            }
+          }
+        });
+
+    gameLoop.getKeyFrames().add(kf);
+    primaryStage.show();
+    gameLoop.play();
 
   }
 
