32dc0d97b151d9a53c37f881a2b25199aa886c0d
KyMaKa <seafth345@gmail.com>
2020-10-03 19:39:02 +0700

    First function

--- CFT_test_task/app/src/main/java/ru/shatalov/cft_test_task/MainActivity.java
+++ CFT_test_task/app/src/main/java/ru/shatalov/cft_test_task/MainActivity.java
@@ -1,36 +1,48 @@
 package ru.shatalov.cft_test_task;
 
-import android.util.Log;
+import android.widget.ListView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-  private TextView message;
-  //protected static BlockingDeque<Currency> currencies = new LinkedBlockingDeque<>();
+  //private TextView message;
+  private String[] listItems;
+  private ReadJson readJson;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-    message = findViewById(R.id.textView);
+    //message = findViewById(R.id.textView);
+
+
     try {
       showCurrencies();
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
 
+    Currency[] currencies = new Currency[readJson.currencies.size()];
+    currencies = readJson.getCurrencies().toArray(currencies);
+
+    MyListAdapter adapter = new MyListAdapter(this, currencies, listItems);
+    ListView listView = findViewById(R.id.listView);
+    listView.setAdapter(adapter);
+
   }
 
 
 
   private void showCurrencies() throws InterruptedException {
-    StringBuilder text = new StringBuilder();
-    ReadJson readJson = new ReadJson();
+    readJson = new ReadJson();
+    ArrayList<String> arrayList = new ArrayList<>();
 
+    //create thread to access source file and get info form it
     Runnable runnable = () -> {
       try {
         readJson.readCurrencyList();
@@ -43,9 +55,10 @@ public class MainActivity extends AppCompatActivity {
     thread.start();
     thread.join(); //make sure that thread execution is finished
     for (Currency currency : readJson.currencies) {
-      text.append(" ").append(currency.getCharCode());
+      arrayList.add(currency.getCharCode());
     }
-    message.setText(text);
-    //System.out.println(currencies.get(2).getCharCode());
+    listItems = new String[arrayList.size()];
+    listItems = arrayList.toArray(listItems);
+    //message.setText("Currencies");
   }
 }
---
+++ CFT_test_task/app/src/main/java/ru/shatalov/cft_test_task/MyListAdapter.java
@@ -0,0 +1,35 @@
+package ru.shatalov.cft_test_task;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MyListAdapter extends ArrayAdapter<String> {
+
+  private final Activity context;
+  private final Currency[] currencies;
+
+  public MyListAdapter(Activity context, Currency[] currencies, String[] currencyChar) {
+    super(context, R.layout.mylist, currencyChar);
+    this.context = context;
+    this.currencies = currencies;
+  }
+  public View getView(int position, View view, ViewGroup parent) {
+    LayoutInflater inflater = context.getLayoutInflater();
+    View rowView = inflater.inflate(R.layout.mylist, null, true);
+
+    TextView titleChar = rowView.findViewById(R.id.currencyChar);
+    TextView titleName =  rowView.findViewById(R.id.currencyName);
+    TextView titleValue = rowView.findViewById(R.id.currencyValue);
+
+    titleChar.setText(currencies[position].getCharCode());
+    titleName.setText(currencies[position].getName());
+    titleValue.setText(currencies[position].getValue() + "");
+
+    return rowView;
+  }
+
+}
